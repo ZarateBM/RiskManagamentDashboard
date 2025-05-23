eb7b9484b1712f94c1f1a01b1a2c2aae0667aa86
¨ZarateBM¨ <brandonzaratem2603@gmail.com>
2025-05-23 12:03:53 -0600

    feat: eliminar migraciones obsoletas y actualizar proveedor de base de datos a PostgreSQL

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,17 +1,20 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-  try {
-    const id = parseInt(params.id)
-    await prisma.usuario.delete({
-      where: { idUsuario: id },
-    })
-    return NextResponse.json({ message: "Usuario eliminado" })
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 500 })
+export async function DELETE(
+  request: NextRequest, 
+  {params}: { params: Promise<{ id: string; }> } ) {
+    const { id } = await params;
+    const userId = parseInt(id, 10);
+    try {
+        await prisma.usuario.delete({
+        where: { idUsuario: userId },
+        })
+        return NextResponse.json({ message: "Usuario eliminado" })
+    } catch (error) {
+        if (error instanceof Error) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+        }
+        return NextResponse.json({ error: "Error al eliminar usuario" }, { status: 500 })
     }
-    return NextResponse.json({ error: "Error al eliminar usuario" }, { status: 500 })
-  }
 }
